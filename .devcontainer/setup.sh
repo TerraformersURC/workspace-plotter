ccb766a7590a63fd8be94b05ae235e7514bf5c01
Abhishekh <126507842+armgits@users.noreply.github.com>
2024-02-15 19:24:02 +0000

    Beautification

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-# Install the KDL library
+# Install the KDL library ------------------------------------------------------
 cd ~/workspace-plotter && git clone https://github.com/orocos/orocos_kinematics_dynamics.git
 mkdir -p orocos_kinematics_dynamics/orocos_kdl/build && cd orocos_kinematics_dynamics/orocos_kdl/build
 
@@ -11,7 +11,7 @@ sudo make install
 
 cd ~/workspace-plotter && rm -rf orocos_kinematics_dynamics
 
-# Install yaml-cpp library
+# Install yaml-cpp library -----------------------------------------------------
 cd ~/workspace-plotter && git clone https://github.com/jbeder/yaml-cpp.git
 mkdir -p yaml-cpp/build && cd yaml-cpp/build
 
