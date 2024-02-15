b42b6d67dfe8861874ab1eadf5a778322a70f535
Abhishekh <126507842+armgits@users.noreply.github.com>
2024-02-15 19:47:45 +0000

    Updated the chain parser function implementation

--- src/workspace_plotter.cpp
+++ src/workspace_plotter.cpp
@@ -2,35 +2,46 @@
 
 #include "workspace_plotter.hpp"
 
-KDL::Chain wspltr::parseYAML(char *yaml_filename)
+KDL::Chain wspltr::chainFromYAML(char *yaml_filename)
 {
   KDL::Chain parsed_chain;
 
   try {
-    YAML::Node links_yaml {YAML::LoadFile(yaml_filename)};
+    YAML::Node arm_node {YAML::LoadFile(yaml_filename)};
 
-    int link_index {1};
+    YAML::Node dh_node {arm_node["dh_values"]};
 
-    for (auto link_map: links_yaml) {
-      std::string link_name {"link" + std::to_string(link_index)};
-      YAML::Node link_node {link_map[link_name]};
+    for (int link_idx {1}; link_idx <= dh_node.size(); link_idx++) {
+      std::string link_name {"link" + std::to_string(link_idx)};
+      YAML::Node link_node {dh_node[link_name]};
 
       auto link_frame {KDL::Frame::DH(link_node["r"].as<double>(),
                                       link_node["alpha"].as<double>(),
                                       link_node["d"].as<double>(),
                                       link_node["theta"].as<double>())};
 
-      // read_parameters.a_length.push_back(link["a_length"].as<double>());
-      // read_parameters.d_length.push_back(link["d_lautoength"].as<double>());
-      // read_parameters.alpha.push_back(link["alpha"].as<double>());
-      // read_parameters.offset.push_back(link["offset"].as<double>());
-      // read_parameters.min_limit.push_back(link["min_limit"].as<double>());
-      // read_parameters.max_limit.push_back(link["max_limit"].as<double>());
+      auto joint_name {"joint" + std::to_string(link_idx)};
+      auto joint_z_axis {KDL::Joint(joint_name, KDL::Joint::RotZ)};
 
-      auto joint_z_axis {KDL::Joint(KDL::Joint::RotZ)};
-      auto link {KDL::Segment(joint_z_axis, link_frame)};
+      auto link {KDL::Segment(link_name, joint_z_axis, link_frame)};
 
       parsed_chain.addSegment(link);
+    }
+  }
+
+  catch(const YAML::BadFile& e) {
+    std::cerr << "Parsing the DH parameters failed" << std::endl;
+    std::cerr << e.msg << std::endl;
+  }
+
+  catch(const YAML::ParserException& e) {
+    std::cerr << "Parsing the DH parameters failed" << std::endl;
+    std::cerr << e.msg << std::endl;
+  }
+
+  std::cout << "Parsed the DH parameters" << std::endl;
+  return parsed_chain;
+}
 
       link_index++;
     }
